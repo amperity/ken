9be375dce01c8c8d136b1ee217ea59b1c28279d9
Greg Look <greg@mvxcvi.com>
2021-05-24 15:47:19 -0700

    Bump SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amperity/ken "0.2.0"
+(defproject com.amperity/ken "0.2.1-SNAPSHOT"
   :description "Observability library to serve as integration with a variety of event outputs."
   :url "https://github.com/amperity/ken"
   :license {:name "MIT License"
