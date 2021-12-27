c2a32abe64fc9d3b77f1fd437cb618add52916ca
Drew Inglis <drew@drewinglis.com>
2021-12-27 15:41:17 -0800

    1.0.2-SNAPSHOT version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.amperity/ken "1.0.1"
+(defproject com.amperity/ken "1.0.2-SNAPSHOT"
   :description "Observability library to serve as integration with a variety of event outputs."
   :url "https://github.com/amperity/ken"
   :license {:name "MIT License"
